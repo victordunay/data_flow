528ba1e7cdc7d4f79444d1fe88bc466619c40fa4
victordunay <victordunay@gmail.com>
2022-12-23 09:34:57 +0200

    add adjacency_matrix

--- dflow_calc.c
+++ dflow_calc.c
@@ -23,3 +23,67 @@ int getProgDepth(ProgCtx ctx) {
 }
 
 
+/* graph template from internet... */
+// Adjacency Matrix representation in C++
+
+#include <iostream>
+using namespace std;
+
+class Graph {
+   private:
+  bool** adjMatrix;
+  int numVertices;
+
+   public:
+  // Initialize the matrix to zero
+  Graph(int numVertices) {
+    this->numVertices = numVertices;
+    adjMatrix = new bool*[numVertices];
+    for (int i = 0; i < numVertices; i++) 
+    {
+      adjMatrix[i] = new bool[numVertices];
+      for (int j = 0; j < numVertices; j++)
+        adjMatrix[i][j] = false;
+    }
+  }
+
+  // Add edges
+  void addEdge(int i, int j) {
+    adjMatrix[i][j] = true;
+    adjMatrix[j][i] = true; // should remove , we need only directed graph
+  }
+
+  // Remove edges
+  void removeEdge(int i, int j) {
+    adjMatrix[i][j] = false;
+    adjMatrix[j][i] = false; // should remove , we need only directed graph
+  }
+
+  // Print the martix
+  void toString() {
+    for (int i = 0; i < numVertices; i++) {
+      cout << i << " : ";
+      for (int j = 0; j < numVertices; j++)
+        cout << adjMatrix[i][j] << " ";
+      cout << "\n";
+    }
+  }
+
+  ~Graph() {
+    for (int i = 0; i < numVertices; i++)
+      delete[] adjMatrix[i];
+    delete[] adjMatrix;
+  }
+};
+
+int main() {
+  Graph g(4);
+
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(1, 2);
+  g.addEdge(2, 0);
+  g.addEdge(2, 3);
+
+  g.toString();
+}
