960285bfcb3fdf0b7a2c2eb7c09a9801a98ebde2
ran <ran-k@campus.technion.ac.il>
2023-01-06 21:09:14 +0200

    wrong output need to check with reference

---
+++ README.txt
@@ -0,0 +1,48 @@
+Hello :)
+This is a test for HW3.
+Please make sure that you add your submission files into this folder.
+
+
+folder structure : 
+
+HW3_tests ->
+
+			examples
+
+			inputs
+				one two three four random
+
+			outputs		
+				one two three four random
+
+			valgrind_tests
+
+			dflow.... (submission files include MAKEFILE!!!!!!)
+
+			run.sh
+
+
+In order to check memory leaks using valgrind uncomment the lines in the script.
+
+STEPS:
+
+make clean
+make
+./run.sh
+
+Hope you all pass :D
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Test written by Yuval Rosman with the help of Nimrod Blecher
--- dflow_calc.c
+++ dflow_calc.c
@@ -1,17 +1,21 @@
 #include "dflow_calc.h"
-int MAX_DEPTH=0;
+#include "stdio.h"
+
+unsigned int MAX_DEPTH=0;
 struct Inst_table * main_table = NULL; 
+unsigned int num_of_inst = 0;
+
 //struct Inst_table *inst_arr = NULL; 
 
 //*****************DATA STRUCTURE*****************************
 //individual instruction
 struct Inst
 {
-   unsigned opcode;
-   unsigned depth;
+   unsigned int opcode;
+   unsigned int depth;
    int dependency_1;
    int dependency_2;
-   unsigned latency;
+   unsigned int latency;
 };
 
 //arr of type struct each index is instruction 
@@ -25,12 +29,16 @@ struct Inst
 
 //*********************UPDATE FUNCTIONS**********************
 //for left src
-unsigned init_1( struct Inst_table * main_table ,unsigned index , int src_index)
+unsigned int init_1( struct Inst_table * main_table ,int index , int src_index)
 {
-    unsigned updated_src_1;
+    //  printf("index=%d\n",index);
+    //  printf("src_index=%d\n",src_index);
+
+    unsigned int updated_src_1;
     if (src_index != -2)//check reg_arr if it depend on other, if not then equal entry
     {
-    main_table->inst_arr[index].dependency_1 = src_index;
+    (main_table->inst_arr[index]).dependency_1 = src_index;
+    updated_src_1 = (main_table->inst_arr[src_index].depth) + (main_table->inst_arr[src_index]).latency; 
     }
     else
     {
@@ -38,17 +46,18 @@ unsigned init_1( struct Inst_table * main_table ,unsigned index , int src_index)
          updated_src_1=0;
     }
     //sum the depth+latency for calc the MAX depth and inst_depth
-     updated_src_1 = main_table->inst_arr[src_index].depth + (main_table->inst_arr[src_index]).latency; 
+    // printf("updated_src_1=%d\n",updated_src_1);
      return updated_src_1;
 }
 
 //for right src
-unsigned init_2(struct Inst_table * main_table,unsigned index, int src_index)
+unsigned int init_2(struct Inst_table * main_table ,unsigned index, int src_index)
 {
-    unsigned updated_src_2;
+    unsigned int updated_src_2;
     if (src_index != -2)//check reg_arr if it depend on other, if not then equal entry
     {
     main_table->inst_arr[index].dependency_2 = src_index;
+    updated_src_2 = main_table->inst_arr[src_index].depth + (main_table->inst_arr[src_index]).latency; 
     }
     else
     {
@@ -56,37 +65,42 @@ unsigned init_2(struct Inst_table * main_table,unsigned index, int src_index)
          updated_src_2=0;//depend on entry so depth =0
     }
     //sum the depth+latency for calc the MAX depth and inst_depth
-     updated_src_2 = main_table->inst_arr[src_index].depth + (main_table->inst_arr[src_index]).latency; 
+    // printf("updated_src_2=%d\n",updated_src_2);
      return updated_src_2;
 }
 
 ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) 
 {
     int tmp=0;
-    
+    num_of_inst=numOfInsts;
     main_table = (struct Inst_table *) malloc(sizeof(struct Inst ) * numOfInsts);
     main_table->inst_arr = malloc(sizeof(struct Inst ) * numOfInsts);
     for (int i = 0; i < 32; i++) 
     {
         main_table->reg_arr[i] = -2; //-1 is entry so NULL is -2
     }
-    for (unsigned i = 0; i < numOfInsts; i++) 
+
+
+
+
+    for (unsigned int i = 0; i < numOfInsts; i++) 
     {
     //fill the database with the input
     main_table->inst_arr[i].opcode = progTrace[i].opcode;
     main_table->inst_arr[i].latency = opsLatency[progTrace[i].opcode];
     //left src
-    unsigned updated_depth_1=init_1( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx] );
+    //printf("reg_arr[i]=%d\n",main_table->reg_arr[progTrace[i].src1Idx]);
+    unsigned int updated_depth_1=init_1( main_table , i , main_table->reg_arr[progTrace[i].src1Idx] );
     //right src
-    unsigned updated_depth_2=init_2( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx] );
+    unsigned int updated_depth_2=init_2( main_table , i , main_table->reg_arr[progTrace[i].src1Idx] );
     //the inst depth is the max between the two src depths (with the latency of the last)
-    if(updated_depth_1 > updated_depth_2)
+    if(updated_depth_1 < updated_depth_2)
     {
-        main_table->inst_arr[i].depth = updated_depth_1;
-        tmp= updated_depth_1;
+        main_table->inst_arr[i].depth = updated_depth_2;
+        tmp= updated_depth_2;
         if (MAX_DEPTH < tmp)
         {
-        MAX_DEPTH =updated_depth_1;
+        MAX_DEPTH =updated_depth_2;
         }
     }
     else
@@ -129,8 +143,8 @@ int getInstDepth(ProgCtx ctx, unsigned int theInst)
   {
     return -1;
   }
-    // return weight_arr[theInst] ;
-    return 0;
+     return main_table->inst_arr[theInst].depth;
+    
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) 
@@ -140,14 +154,23 @@ int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2De
    {
     return -1;
    }
-//    *src1DepInst = inst_arr[theInst].dependency_1; 
-//    *src2DepInst = inst_arr[theInst].dependency_2;
+    *src1DepInst = main_table->inst_arr[theInst].dependency_1; 
+    *src2DepInst = main_table->inst_arr[theInst].dependency_2;
     return 0;   
 }
 
 int getProgDepth(ProgCtx ctx) 
 {
 return MAX_DEPTH;
+    // int max_depth = 0;
+    // for ( int i = 0; i < num_of_inst; i++) 
+    // {
+    //     if ((   (  main_table->inst_arr[i]).depth + (main_table->inst_arr[i]).latency) > max_depth)
+    //      {
+    //         max_depth = ((main_table->inst_arr[i]).depth + (main_table->inst_arr[i]).latency);
+    //     }
+    // }
+    // return max_depth;
 }
 
 
