55608793a4e73da0b18be32a84dabd06316c8cf4
ran <ran-k@campus.technion.ac.il>
2023-01-06 17:14:53 +0200

    updtae , less compile errors

--- dflow_calc.c
+++ dflow_calc.c
@@ -1,28 +1,27 @@
 #include "dflow_calc.h"
 int MAX_DEPTH=0;
+struct Inst_table * main_table = NULL; 
+struct Inst_table *inst_arr = NULL; 
+
 //*****************DATA STRUCTURE*****************************
 //individual instruction
-struct Inst
+ struct Inst
 {
    unsigned opcode;
    unsigned depth;
    int dependency_1;
    int dependency_2;
    unsigned latency;
-};
+}Inst;
 
 //arr of type struct each index is instruction 
-struct Inst_table
+ struct Inst_table
 {
+    int reg_arr[32];
     struct Inst *inst_arr; 
     inst_arr = (struct Inst_table *)malloc(sizeof(struct Inst ) * numOfInsts);
-    int reg_arr[32];
-    unsigned num_of_inst = numOfInsts; 
-        for (int i = 0; i < 32; i++) 
-        {
-            reg_arr[i] = -2; //-1 is entry so NULL is -2
-        }
-};
+}Inst_table;
+
 
 //*********************UPDATE FUNCTIONS**********************
 //for left src
@@ -65,15 +64,19 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
 {
     int tmp=0;
     main_table = (struct Inst_table *) malloc(sizeof(struct Inst ) * numOfInsts);
+    for (int i = 0; i < 32; i++) 
+    {
+        main_table->reg_arr[i] = -2; //-1 is entry so NULL is -2
+    }
     for (unsigned i = 0; i < numOfInsts; i++) 
     {
     //fill the database with the input
     main_table->inst_arr[i].opcode = progTrace[i].opcode;
     main_table->inst_arr[i].latency = opsLatency[progTrace[i].opcode];
     //left src
-    unsigned updated_depth_1=init_1( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx]);
+    unsigned updated_depth_1=init_1( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx] );
     //right src
-    unsigned updated_depth_2=init_2( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx]);
+    unsigned updated_depth_2=init_2( main_table , i ,main_table->reg_arr[progTrace[i].src1Idx] );
     //the inst depth is the max between the two src depths (with the latency of the last)
     if(updated_depth_1 > updated_depth_2)
     {
